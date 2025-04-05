aab0f92cb31dd13839476b302fbeb811a0a365a3
Akhilesh Yadav <ydvakhilesh16@gmail.com>
2025-04-06 01:20:32 +0530

    add useParams in creator id page

--- src/app/creator/[id]/page.tsx
+++ src/app/creator/[id]/page.tsx
@@ -1,11 +1,11 @@
+"use client";
+
+import { useParams } from "next/navigation";
 import StreamView from "@/components/StreamView";
 
-type Props = {
-  params: {
-    creatorId: string;
-  };
-};
+export default function CreatorPage() {
+  const params = useParams();
+  const creatorId = params.id as string;
 
-export default function CreatorPage({ params }: Props) {
-  return <StreamView creatorId={params?.creatorId} />;
+  return <StreamView creatorId={creatorId} />;
 }
